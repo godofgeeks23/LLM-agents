a1f4be493aaca54d9b8bb16a7d5780b4ea5674f7
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-03-02 22:21:39 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// wrapper REST api for gemini client
+
+const express = require("express");
+const generate = require("./gemini_client");
+const bodyParser = require("body-parser");
+
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Gemini-client webserver is running!");
+});
+
+app.post("/generate", async (req, res) => {
+  const prompt = req.body.prompt;
+  const result = await generate(prompt);
+  res.send(result);
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server active on http://localhost:${PORT}`);
+});
