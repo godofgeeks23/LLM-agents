17fdaa6264880196d7c8fde41332315d6c662e43
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-03-04 23:31:44 +0530

    Update agent.js

--- agent.js
+++ agent.js
@@ -19,9 +19,31 @@ function getWeatherDetails(city = '') {
     }
 }
 
+const AGENT_PROMPT = `
+You are an AI Assistant with START, PLAN, ACTION, OBSERVATION and OUTPUT State.
+Wait for the user prompt and first PLAN using available tools.
+After Planning, Take the ACTION with appropriate tools and wait for OBSERVATION based on ACTION.
+Once you get the observations, Return the AI response based on START prompt and observations
+
+Available tools - 
+1. getWeatherDetails(city: string) : string - Returns temperature and humidity for the city
+
+Example:
+START
+{ "type": "user", "user": "What is the sum of weather of Bangalore and Chennai?" }
+{ "type": "plan", "plan": "I will call the getWeatherDetails for Bangalore" }
+{ "type": "action", "function": "getWeatherDetails", "input": "Bangalore" }
+{ "type": "observation", "observation": "10°C" }
+{ "type": "plan", "plan": "I will call getWeatherDetails for Chennai" }
+{ "type": "action", "function": "getWeatherDetails", "input": "Chennai" }
+{ "type": "observation", "observation": "14°C" }
+{ "type": "output", "output": "The sum of weather of Bangalore and Chennai is 24°C"
+     `;
+
 async function main() {
   
-  const prompt = `What is the current weather in Bangalore?`;  
+//   const prompt = `What is the weather in Bangalore?`;  
+const prompt = "Who are you?";
 
   const result = await generate(prompt);
   console.log(result);
